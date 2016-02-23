fa5fbb0b9fce9a7d207920758734f4e435fed056
Piromsurang Rungserichai <Gift@Piromsurangs-MacBook-Pro.local>
2016-02-23 18:54:48 +0900

    space ship can move now

--- src/GameLayer.js
+++ src/GameLayer.js
@@ -3,12 +3,37 @@ var GameLayer = cc.LayerColor.extend({
         this._super( new cc.Color( 127, 127, 127, 255 ) );
         this.setPosition( new cc.Point( 0, 0 ) );
         
-        var ship = new Ship();
-        ship.setPosition( new cc.Point( 200, 200 ) );
-        this.addChild( ship );
-        ship.scheduleUpdate();
+        this.ship = new Ship();
+        this.ship.setPosition( new cc.Point( 200, 220 ) );
+        this.addChild( this.ship );
+        this.ship.scheduleUpdate();
+        this.addKeyboardHandlers();
+
         
         return true;
+    }, 
+    
+    onKeyDown: function( keyCode, event ) {
+		if ( keyCode == cc.KEY.space ) {
+	       this.ship.switchDirection();
+       }
+    },
+    
+    onKeyUp: function( keyCode, event ) {
+	   console.log( 'Up: ' + keyCode.toString() );
+    },
+    
+    addKeyboardHandlers: function() {
+        var self = this;
+        cc.eventManager.addListener({
+            event: cc.EventListener.KEYBOARD,
+            onKeyPressed : function( keyCode, event ) {
+                self.onKeyDown( keyCode, event );
+            },
+            onKeyReleased: function( keyCode, event ) {
+                self.onKeyUp( keyCode, event );
+            }
+        }, this);
     }
 });
  
--- src/Ship.js
+++ src/Ship.js
@@ -1,16 +1,39 @@
+
 var Ship = cc.Sprite.extend({
     ctor: function() {
         this._super();
         this.initWithFile( 'res/images/ship.png' );
+        this.direction = Ship.DIR.UP;   
     },
     update: function( dt ) {
         var pos = this.getPosition();
-        
-        if ( pos.y < screenHeight ) {
+        if ( pos.y < screenHeight && this.direction == Ship.DIR.UP ) {
 	       this.setPosition( new cc.Point( pos.x, pos.y + 5 ) );
 	    } 
-        else {
+        else if (pos.y >= screenHeight){
 	       this.setPosition( new cc.Point( pos.x, 0 ) );
         }
+        
+        else if ( pos.x < screenWidth && this.direction == Ship.DIR.RIGHT) {
+            this.setPosition( new cc.Point( pos.x + 5, pos.y ) );
+        }
+        else if ( pos.x >= screenWidth ){
+            this.setPosition( new cc.Point( 0, pos.y ) );
+        }
+    },
+    switchDirection: function() {
+	   if ( this.direction == Ship.DIR.UP ) {
+           this.direction = Ship.DIR.RIGHT;
+           this.setRotation( 90 );
+	   } 
+        else {
+           this.direction = Ship.DIR.UP;
+           this.setRotation( 0 );
+	   }
     }
 });
+
+Ship.DIR = {
+    UP: 1,
+    RIGHT: 2
+};
